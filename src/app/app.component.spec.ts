28e08a03492d665b0e4718342e059101c55cdad2
getulio.souza <getulio.souza@evolutix.com.br>
2025-05-01 18:46:06 -0300

    feature: working-on-temlate-driven-form

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,10 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, CommonModule, FormsModule],
     }).compileComponents();
   });
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'template-driven-form';
+  disabledBtn: boolean = false;
+
+  @ViewChild('registrationForm') form!: NgForm
+
+    ngOnInit(): void {
+        console.log(this.form)
+    }
+
+  onFormSubmitted(form: NgForm) {
+    console.log(this.form.valid)
+    // console.log(this.form.controls['firstName'].value);
+    // console.log(this.form.controls['lastName'].value);
+    this.disabledBtn = true;
+  }
+
+  isFormValid() {
+    return this.form?.valid ?? false;
+  }
 }
